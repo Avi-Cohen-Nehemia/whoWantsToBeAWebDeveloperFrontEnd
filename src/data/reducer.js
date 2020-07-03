5b4365ce3a3b7d41081b182b28f6b9cb3ef240f2
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-07-03 12:05:25 +0100

    installed react-redux

--- src/data/reducer.js
+++ src/data/reducer.js
@@ -1,7 +1,6 @@
 //importing initialState to make it available to the reducer
 import initialState from "./initialState";
 
-
 const reducer = (state, action) => {
     switch (action.type) {
         case "RESET_GAME": return initialState;
