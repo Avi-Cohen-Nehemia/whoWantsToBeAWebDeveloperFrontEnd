13b5805a5654a2c6d5257bdd32090ba990839ae5
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-07-03 15:30:49 +0100

    changed Question to a class base component and added state

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Question = () => (
-    <>
-        <h2></h2>
-        <h4></h4>
-        <h4></h4>
-        <h4></h4>
-        <h4></h4>
-    </>
-);
+class Question extends Component {
 
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            playerName: props.playerName,
+            question: props.currentQuestion.content,
+
+            answerA: props.currentQuestion.answerA.answer,
+            isCorrectA: props.currentQuestion.answerA.correct,
+
+            answerB: props.currentQuestion.answerB.answer,
+            isCorrectB: props.currentQuestion.answerB.correct,
+
+            answerC: props.currentQuestion.answerC.answer,
+            isCorrectC: props.currentQuestion.answerC.correct,
+
+            answerD: props.currentQuestion.answerD.answer,
+            isCorrectD: props.currentQuestion.answerD.correct,
+
+            selected: 0,
+        };
+
+        // this.handlePlayerName = this.handlePlayerName.bind(this);
+    }
+
+    // handlePlayerName(e) {
+    //     this.setState({ playerName: e.currentTarget.value });
+    // }
+
+    render() {
+        return(
+            <>
+                <h2></h2>
+                <h4></h4>
+                <h4></h4>
+                <h4></h4>
+                <h4></h4>
+            </>
+        );
+    }
+}
+  
 export default Question;
--- src/data/initialState.js
+++ src/data/initialState.js
@@ -1,5 +1,27 @@
 export const initialState = {
+    playerName: "",
+    currentQuestion: {
+        content: "",
+        answerA: {
+            answer: "",
+            correct: null
+        },
 
+        answerB: {
+            answer: "",
+            correct: null
+        },
+
+        answerC: {
+            answer: "",
+            correct: null
+        },
+        
+        answerD: {
+            answer: "",
+            correct: null
+        }
+    }
 };
 
 export default initialState;
