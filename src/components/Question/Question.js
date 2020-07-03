22312cb508bd212c89965b1495f5afecb28ab76c
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-07-03 15:05:36 +0100

    created Question component boilerplate

---
+++ src/components/Question/Question.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Question = () => (
+    <>
+        <h2></h2>
+        <h4></h4>
+        <h4></h4>
+        <h4></h4>
+        <h4></h4>
+    </>
+);
+
+export default Question;
