bad8f0490652c4c7e1eeacb6c5dd6c899ea5c95c
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-07-22 16:58:23 +0100

    baseURL in the axios file changed to point to the deployed aws machine

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // return a new version of axios with useful settings applied
 export default axios.create({
-        baseURL: "http://192.168.10.10/api",
+        baseURL: "http://ec2-18-130-184-16.eu-west-2.compute.amazonaws.com/api",
         headers: {
         Accept: "application/json",
     },
