f600b7a6dbcd9b4d7d6c2fae99ea8a5eccea99bb
mdm106 <mads.mcmahon@googlemail.com>
2020-07-22 12:30:01 +0100

    Adjustments made to bring reset button forward in congratulations alert and fireworks still visible

--- src/components/Fireworks/index.js
+++ src/components/Fireworks/index.js
@@ -3,9 +3,9 @@ import React from "react";
 import "./../../assets/css/fireworks.css";
 
 const Fireworks = () => (
-    <div class="pyro">
-        <div class="before"></div>
-        <div class="after"></div>
+    <div className="pyro">
+        <div className="before"></div>
+        <div className="after"></div>
     </div>
 )
 
