1fb46023ef4d94cf688973b90b9f08ea24d99a31
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-07-23 10:18:54 +0100

    congratulations sound stays muted and now has a higher z-index

--- src/components/CongratulationsAlert/CongratulationsAlert.js
+++ src/components/CongratulationsAlert/CongratulationsAlert.js
@@ -5,18 +5,20 @@ import Reset from "./../Reset";
 import Fireworks from "./../Fireworks";
 import applause from "./../../assets/sounds/crowdapplause.mp3";
 
-const CongratulationsAlert = ({ statistics, playerName }) => (
+const CongratulationsAlert = ({ statistics, playerName, isMuted }) => (
     <>
         <p>Congratulations { playerName }!<br/>
         You are a Web Developer!</p>
         <p>Well done! Only { 100 - statistics }% of players have won the game!</p>
         <Reset btnStyle={"winning-button"} />
         <Fireworks />
+        { !isMuted ?
         <Sound
             url={ applause }
             playStatus={ Sound.status.PLAYING }
             autoLoad={ true }
         />
+        : null }
     </>
 )
 
