61a6caaa66527240af66d3ea982b38afff37cd05
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-07-23 16:30:42 +0100

    changed baseURL to who-wants-to-be-a-web-developer.developme.space/api

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // return a new version of axios with useful settings applied
 export default axios.create({
-        baseURL: "http://ec2-18-130-184-16.eu-west-2.compute.amazonaws.com/api",
+        baseURL: "https://who-wants-to-be-a-web-developer.developme.space/api",
         headers: {
         Accept: "application/json",
     },
