9ac422c2f74c89ad3483b25212e4884881f3a663
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-07-23 11:04:21 +0100

    WrongAnswerAlert gives statistics only from 2nd question

--- src/components/WrongAnswerAlert/WrongAnswerAlert.js
+++ src/components/WrongAnswerAlert/WrongAnswerAlert.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Reset from "./../Reset";
 
-const WrongAnswerAlert = ({ playerName, correctAnswer, statistics }) => (
+const WrongAnswerAlert = ({ playerName, correctAnswer, statistics, currentDifficulty }) => (
     <>
         <p>Bad luck { playerName }, you're wrong! The right answer is { correctAnswer }</p>
+        { currentDifficulty !== 1 ?
         <p>Well done though, you did better than { statistics }% of the people who played before!</p>
+        : null}
         <Reset btnStyle={"winning-button"} />
     </>
 )
--- src/components/WrongAnswerAlert/index.js
+++ src/components/WrongAnswerAlert/index.js
@@ -5,6 +5,7 @@ const mapStateToProps = (state) => {
     return {
         playerName: state.playerName,
         statistics: state.statistics,
+        currentDifficulty: state.currentDifficulty,
     };
 };
 
